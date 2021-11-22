00e65845977d371992d2db96caf88956f3f5fd83
nmfuadi <nmfuadi@gmail.com>
2021-11-22 16:04:56 +0700

    update lagi

--- application/views/Report/Finance/RCashFlowPerbulan.php
+++ application/views/Report/Finance/RCashFlowPerbulan.php
@@ -23,8 +23,8 @@
 
                         <div class="row" style="margin-bottom: 10px">
                             <div class="col-md-4">
-                                <h3 class="box-title m-b-0">Report Cashflow <?php echo $start .' S/d '.$end ?></h3>
-                                <p class="text-muted m-b-30">Report Cashflow <?php echo $start .' S/d '.$end ?></p>
+                                <h3 class="box-title m-b-0">Report Cashflow <?php echo date_format($start, "d/m/y") .' S/d '. date_format($end, "d/m/y") ?></h3>
+                                <p class="text-muted m-b-30">Report Cashflow <?php echo date_format($start, "d/m/y") .' S/d '. date_format($end, "d/m/y") ?></p>
                             </div>
                             <div class="col-md-4 text-center">
                                 <div style="margin-top: 8px" id="message">
